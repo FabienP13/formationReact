4eac5dd325c02fa8a5a5d60e901c5a6530d640d7
Fabien PONCET <fponcet@centumadeneo.com>
2026-05-27 08:54:20 +0200

    [formationReact] - hook useState

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # React + Vite
 
-Formation react
+Formation React
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+
 
 function App() {
-  return 'Bonjour les gens'
+  //HOOK : Ne pas faire de condition sur les hooks, ils doivent être appelés à chaque rendu du composant dans le même ordre
+  const [count, setCount] = useState(0);
+  const [person, setPerson] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    age: 18,
+  });
+
+  const incrementAge = () => {
+    // setCount(count + 1);
+    setPerson((person) => ({ ...person, age: person.age + 1 }));
+  }
+   const incrementCount= () => {
+    setCount(count + 1);
+    
+  }
+  return (
+    <>
+      <p>Age de {person.firstName} {person.lastName} : {person.age}</p>
+      <button onClick={incrementAge}>Gagner une année</button>
+      <button onClick={incrementCount}>Incrémenter {count}</button>
+    </>
+  );
 }
 
-export default App
+export default App;
